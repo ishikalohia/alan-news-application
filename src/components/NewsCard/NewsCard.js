12241a823bd0bc918da376d8239114e3de418b8f
Ishika <ishikalohia4@gmail.com>
2020-11-08 16:25:13 +0530

    Content added in Cards

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -9,34 +9,42 @@ import {
   Typography,
 } from "@material-ui/core";
 
-const NewsCard = () => {
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
   return (
     <Card>
       <CardActionArea>
-        <CardMedia />
+        <CardMedia
+          image={
+            urlToImage ||
+            "https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-creative-breaking-news-tag-png-image_4265318.jpg"
+          }
+        />
         <div>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="h2"
-          ></Typography>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="h2"
-          ></Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
         </div>
-        <Typography gutterBottom variant="h5"></Typography>
+        <Typography gutterBottom variant="h5">
+          {title}
+        </Typography>
         <CardContent>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="p"
-          ></Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small" color="primary"></Button>
-          <Typography variant="h5" color="textSecondary"></Typography>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+          <Typography variant="h5" color="textSecondary">
+            {i + 1}
+          </Typography>
         </CardActions>
       </CardActionArea>
     </Card>
