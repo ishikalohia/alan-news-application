69aba50daea66322f60d12d436d289adbc1e0cce
ishikalohia <58163099+ishikalohia@users.noreply.github.com>
2020-11-18 16:19:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import Hero from "./Components/Hero";
+import Hero from "./components/Hero";
 import wordstonumbers from "words-to-numbers";
-import NewsCards from "./Components/NewsCards/NewsCards";
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey =
   "61a5b012b3a91f4bbd0b5747715b60952e956eca572e1d8b807a3e2338fdd0dc/stage";
